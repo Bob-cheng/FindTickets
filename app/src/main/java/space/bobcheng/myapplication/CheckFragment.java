fd2e55ee796423178217a0292d24473db2d1f79e
bobcheng <bobchengzy@gmail.com>
2017-03-30 00:30:13 +0800

    2017.3.29

--- app/src/main/java/space/bobcheng/myapplication/CheckFragment.java
+++ app/src/main/java/space/bobcheng/myapplication/CheckFragment.java
@@ -1,17 +1,24 @@
 package space.bobcheng.myapplication;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.design.widget.TextInputLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CheckFragment extends Fragment {
+    private EditText start_time;
 
 
     public CheckFragment() {
@@ -26,4 +33,27 @@ public class CheckFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_check, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Calendar c = Calendar.getInstance();
+        start_time = (EditText) getView().findViewById(R.id.start_date);
+        start_time.setText(c.get(Calendar.YEAR)+"年"+(c.get(Calendar.MONTH)+1)+"月"+c.get(Calendar.DAY_OF_MONTH)+"日");
+        start_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                new DatePickerDialog(getActivity(),
+                                    new DatePickerDialog.OnDateSetListener(){
+                                        @Override
+                                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                            start_time.setText(year+"年"+(month+1)+"月"+dayOfMonth+"日");
+                                        }
+                                    }
+                                    , c.get(Calendar.YEAR)
+                                    , c.get(Calendar.MONTH)
+                                    , c.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
 }
